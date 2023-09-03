3eb2483c68b9cc9670cdf888e65317c27af45dd5
juliansbaglia18 <julysbaglia18@gmail.com>
2023-09-03 20:42:27 -0300

    Agrego proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejercicios';
+  public edadUno: number = 0;
+  public edadDos: number = 0;
+  public promedio: number = 0;
+  public suma: number = 0;
+
+  calcular() {
+    this.promedio = (this.edadUno + this.edadDos) / 2;
+    this.suma = this.edadUno + this.edadDos;
+  }
+
+  limpiar() {
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.promedio = 0;
+    this.suma = 0;
+  }
 }
